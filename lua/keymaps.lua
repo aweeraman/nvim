f433faf19c416342d878d8c2f70e79b09457130f
Anuradha Weeraman <anuradha@debian.org>
2024-01-16 14:50:52 +0530

    Basic settings and keymaps

---
+++ lua/keymaps.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.keymap.set('n', '<leader>v', ':vsplit<CR>')
+vim.keymap.set('n', '<leader>h', ':split<CR>')
---
+++ lua/settings.lua
@@ -0,0 +1,15 @@
+-- Enable plugin features
+vim.cmd("syntax on")
+vim.cmd("filetype plugin indent on")
+
+-- Set basic options
+vim.o.hidden = true
+vim.o.title = true
+vim.o.cursorline = true
+vim.bo.tabstop = 8
+vim.bo.softtabstop = 8
+vim.bo.shiftwidth = 8
+vim.bo.expandtab = false
+vim.wo.number = true
+vim.o.mouse = "a"
+vim.o.termguicolors = true
