d48dc43b41551fcbf37e4c798173d8cb3b9172d2
Anuradha Weeraman <anuradha@debian.org>
2024-01-16 15:05:08 +0530

    Changed settings to use the vim.opt* variants

--- lua/settings.lua
+++ lua/settings.lua
@@ -3,13 +3,13 @@ vim.cmd("syntax on")
 vim.cmd("filetype plugin indent on")
 
 -- Set basic options
-vim.o.hidden = true
-vim.o.title = true
-vim.o.cursorline = true
-vim.bo.tabstop = 8
-vim.bo.softtabstop = 8
-vim.bo.shiftwidth = 8
-vim.bo.expandtab = false
-vim.wo.number = true
-vim.o.mouse = "a"
-vim.o.termguicolors = true
+vim.opt.hidden = true
+vim.opt.title = true
+vim.opt.cursorline = true
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.expandtab = false
+vim.opt.number = true
+vim.opt.mouse = "a"
+vim.opt.termguicolors = true
