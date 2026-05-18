086de0e1c9f32df0cb115d49ae6538514c670fc0
Anuradha Weeraman <anuradha@debian.org>
2026-05-18 23:38:15 +0530

    Dashboard page

--- init.lua
+++ init.lua
@@ -155,9 +155,32 @@ require("snacks").setup({
 		sections = {
 			{ section = "header" },
 			{ section = "keys", gap = 1, padding = 1 },
+			{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+			{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+			{
+				pane = 2,
+				icon = " ",
+				title = "Git Status",
+				section = "terminal",
+				enabled = function()
+					return Snacks.git.get_root() ~= nil
+				end,
+				cmd = "git status --short --branch --renames",
+				height = 5,
+				padding = 1,
+				ttl = 5 * 60,
+				indent = 3,
+			},
 		},
 	},
 	explorer = { enabled = true },
+	image = {
+		enabled = true,
+		doc = {
+			inline = true,
+			render_all = true,
+		},
+	},
 	indent = { enabled = true },
 	input = { enabled = true },
 	picker = { enabled = true },
