0e2fc4b739cd2bfb255c38cfa688fba4ad9a1bba
Anuradha Weeraman <anuradha@debian.org>
2026-05-18 22:52:53 +0530

    Added snacks.nvim

--- init.lua
+++ init.lua
@@ -139,6 +139,36 @@ require("tokyonight").setup()
 -- set the theme
 vim.cmd.colorscheme("onedark")
 
+-- snacks.nvim
+
+vim.pack.add({
+	{
+		src = "https://github.com/folke/snacks.nvim",
+		version = "v2.31.0",
+	}
+})
+
+require("snacks").setup({
+	bigfile = { enabled = true },
+	dashboard = {
+		enabled = true,
+		sections = {
+			{ section = "header" },
+			{ section = "keys", gap = 1, padding = 1 },
+		},
+	},
+	explorer = { enabled = true },
+	indent = { enabled = true },
+	input = { enabled = true },
+	picker = { enabled = true },
+	notifier = { enabled = true },
+	quickfile = { enabled = true },
+	scope = { enabled = true },
+	scroll = { enabled = true },
+	statuscolumn = { enabled = true },
+	words = { enabled = true },
+})
+
 -- neo-tree
 
 vim.pack.add({
